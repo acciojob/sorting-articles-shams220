edb45198b226ee43bc006d62f0ba69b3ac6d196a
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-27 11:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 //your JS code here. If required.
 const ar = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']
+const sortedBands = ar.sort((a, b) => {
+ 
+  const strip = (name) => name.replace(/^(a |an |the )/i, '').trim();
+  return strip(a) > strip(b) ? 1 : -1;
+});
 const ul = document.querySelector('#band');
-ar.map((i)=>{ 
+sortedBands.map((i)=>{ 
     let li = document.createElement('li')
     li.textContent = i;
     ul.appendChild(li)
