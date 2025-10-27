8ad6e7447a3c10c00192b5514f957523dee9577f
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-27 11:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const ar = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']
+const ul = document.querySelector('#band');
+ar.map((i)=>{ 
+    let li = document.createElement('li')
+    li.textContent = i;
+    ul.appendChild(li)
+}
+)
+document.querySelector('body').appendChild(ul)
